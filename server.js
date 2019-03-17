7b2f29e690012149fb59f8ba84f9a20d5905df44
Avinash <avinashdhillor24@gmail.com>
2019-03-18 00:36:42 +0530

    all routes called in main server.js

---
+++ server.js
@@ -0,0 +1,27 @@
+const dotenv = require('dotenv').config();
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const mongoose = require('./db/connection');
+
+//Routes
+//@desc All routes defined in routes/api/
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+const items = require('./routes/api/items');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+app.use('/api/items', items);
+
+app.listen(PORT, err => {
+  if (err) {
+    console.log('Error connecting with port');
+  }
+  console.log(`Listening on Port ${PORT}`);
+});
