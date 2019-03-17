c05b52b6916ebf52f7eec089fb0ade332d66fd26
Avinash <avinashdhillor24@gmail.com>
2019-03-18 04:10:15 +0530

    signup route added

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express.Router();
 
+const validateSignup = require('../../validations/users/validateSignup');
+
 router.get('/', (req, res) => {
   res.send('Users route working');
+});
+
+router.post('/signup', (req, res) => {
+  const { errors, isValid } = validateSignup(req.body);
+  if (!isValid) {
+    res.status(400).send(errors);
+  }
 });
 
 module.exports = router;
