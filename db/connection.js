312adfcc4d394a0ef1654d334057e24562296c9d
Avinash <avinashdhillor24@gmail.com>
2019-03-18 00:34:01 +0530

    warning fixed

--- db/connection.js
+++ db/connection.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI, err => {
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true }, err => {
   if (err) {
     console.log('Error connecting with database');
   }
